20ed12ef8a6c86a1077365b869e811f0ff2af0c6
Harry Zhang <hnzhang@gmail.com>
2021-01-06 14:14:51 -0800

    update web page title

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var data = {
-  title: 'Hello World!',
+  title: 'Hello World, You!',
   selected: null,
   question: 'In the Battle of Wolf 359 what prominent Miranda-class ship was destroyed?',
   choices: [
